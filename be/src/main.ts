1869cbf11849f4bf8efdad4de897def072875ff1
Erikyy <eriky@hotmail.com>
2021-04-19 10:59:55 +0300

    BE: Add nest project with graphql

---
+++ be/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log("\u001b[1;32mHosting app on: http://localhost:3000");
+  
+}
+bootstrap();
