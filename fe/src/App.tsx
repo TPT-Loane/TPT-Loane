1e646f10220e47ec400391e882e85e89d648ecba
tomimarkus991 <tomimarkusalber@gmail.com>
2021-04-20 10:53:01 +0300

    update folder structure

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/pages/Home';
-import About from './components/pages/About/index';
-import Error from './components/pages/Error/index';
+import Home from './pages/Home';
+import About from './pages/About/index';
+import Error from './pages/Error/index';
 
 function App(): JSX.Element {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
         <Route component={Error} />
       </Switch>
     </Router>
