8bba50773bb2e241d576045a59052673983c0503
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2021-04-23 12:13:00 +0300

    Add Header layout to App

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/index';
 import About from './pages/About/index';
 import Error from './pages/Error/index';
+import Header from './components/layouts/Header';
 
 function App(): JSX.Element {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route component={Error} />
-      </Switch>
-    </Router>
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={Error} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
