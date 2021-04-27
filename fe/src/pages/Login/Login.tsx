0ee26964fd889ac84240e0ca61fa82c996cc6e86
Hendrik Metsallik <hendrik.metsallik@tptlive.ee>
2021-04-27 16:32:07 +0300

    Changed login page to Chakra

--- fe/src/pages/Login/Login.tsx
+++ fe/src/pages/Login/Login.tsx
@@ -1,16 +1,17 @@
 import styles from './Login.module.scss';
+import { Input, Button, Box, Text } from "@chakra-ui/react";
 
 function Login(): JSX.Element {
     return (
-        <div className={styles.login}>
-            <div className={styles.loginItems}>
-                <h1 className={styles.h1}>Login</h1>
-                <input className={styles.input} type="text" placeholder="example@tptlive.ee" id="username"/>
-                <input className={styles.input} type="text" placeholder="password" id="password"/>
-                <button className={styles.button}>Login</button>
-            </div>
+        <Box className={styles.login}>
+            <Box className={styles.loginItems}>
+                <Text fontSize="30px" mb="30px">Login</Text>
+                <Input mb="30px" w="30%" type="text" placeholder="example@tptlive.ee" id="username"/>
+                <Input mb="20px" w="30%" type="text" placeholder="password" id="password"/>
+                <Button>Login</Button>
+            </Box>
             
-        </div>
+        </Box>
     );
 }
 export default Login;
