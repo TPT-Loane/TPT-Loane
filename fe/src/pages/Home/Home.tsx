604ec8a1c9b7e097bf665de0e9ebae985185c64f
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2021-04-27 11:15:21 +0300

    Clean up Home page

--- fe/src/pages/Home/Home.tsx
+++ fe/src/pages/Home/Home.tsx
@@ -1,14 +1,11 @@
-import { Flex, Image } from '@chakra-ui/react';
-import logo from '../../assets/logo.svg';
-import AppLink from './AppLink';
+import {
+  Box,
+} from '@chakra-ui/react';
 import styles from './Home.module.scss';
 
 function Home(): JSX.Element {
   return (
-    <Flex w="100%" minH="100vh" justifyContent="center" flexDirection="column" alignItems="center">
-      <Image src={logo} className={styles.homeLogo} alt="logo" />
-      <AppLink />
-    </Flex>
+    <Box className={styles.home} />
   );
 }
 
