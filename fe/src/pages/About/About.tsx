aba7f63ce94338b74a8f566ca08fa2f788e5318d
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2021-04-30 11:53:42 +0300

    Fix sidebar not being full height in some pages

--- fe/src/pages/About/About.tsx
+++ fe/src/pages/About/About.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from '@chakra-ui/react';
 function About(): JSX.Element {
   // @TODO - Add some actual content here.
   return (
-    <Box minH="100vh">
+    <Box>
       <Text fontSize="xl">About Page</Text>
     </Box>
   );
