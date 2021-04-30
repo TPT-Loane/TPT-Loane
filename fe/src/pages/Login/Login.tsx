003cad7153b2ca1b0fa004a9e9786416a80dc76d
Hendrik Metsallik <hendrik.metsallik@tptlive.ee>
2021-04-30 09:07:29 +0300

    Removed all scss for login page

--- fe/src/pages/Login/Login.tsx
+++ fe/src/pages/Login/Login.tsx
@@ -1,17 +1,16 @@
-import styles from './Login.module.scss';
-import { Input, Button, Box, Text } from "@chakra-ui/react";
+// import styles from './Login.module.scss';
+import { Input, Button, Flex, Text } from "@chakra-ui/react";
 
 function Login(): JSX.Element {
     return (
-        <Box className={styles.login}>
-            <Box className={styles.loginItems}>
-                <Text fontSize="30px" mb="30px">Login</Text>
-                <Input mb="30px" w="30%" type="text" placeholder="example@tptlive.ee" id="username"/>
-                <Input mb="20px" w="30%" type="text" placeholder="password" id="password"/>
-                <Button>Login</Button>
-            </Box>
-            
-        </Box>
+        <Flex direction="column" align="center">
+            <Text fontSize="30px" mb="30px">Login</Text>
+            <Input mb="30px" w="30%" type="text" placeholder="example@tptlive.ee" id="username" />
+            <Input mb="30px" w="30%" type="text" placeholder="password" id="password" />
+            <Button>Login</Button>
+        </Flex>
+
+
     );
 }
 export default Login;
