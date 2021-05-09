89f265bdbd3e3b7c4d50253e176c465590de6b0d
Orzelius <albertkostusev@gmail.com>
2021-05-10 01:34:31 +0300

    Add category service

---
+++ be/src/category/category.service.ts
@@ -0,0 +1,56 @@
+import { Category } from './entities/category.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateCategoryInput } from './dto/create-category.input';
+import { Connection, Repository } from 'typeorm';
+import { UpdateCategoryInput } from './dto/update-category.input';
+import { NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepo: Repository<Category>,
+    private readonly connection: Connection,
+  ) {}
+
+  async create(createCategoryInput: CreateCategoryInput) {
+    const parentCategory = await this.categoryRepo.findOne(
+      createCategoryInput.parentCategoryId,
+    );
+    if (!parentCategory) {
+      throw new NotFoundException(
+        `Category #${createCategoryInput.parentCategoryId} not found (parent parameter)`,
+      );
+    }
+
+    const category = this.categoryRepo.create({
+      bundelable: createCategoryInput.bundelable,
+      name: createCategoryInput.name,
+      parentCategory: parentCategory,
+    });
+
+    return this.categoryRepo.save(category);
+  }
+
+  async findAll() {
+    return this.categoryRepo.find({
+      relations: ['subCategories'],
+    });
+  }
+
+  async update(id: number, updateCategoryInput: UpdateCategoryInput) {
+    const category = await this.categoryRepo.preload({
+      id,
+      ...updateCategoryInput,
+    });
+    if (!category) throw new NotFoundException(`Category #${id} not found`);
+    return this.categoryRepo.save(category);
+  }
+
+  async remove(id: number) {
+    const category = await this.categoryRepo.findOne(id);
+    if (!category) throw new NotFoundException(`Category #${id} not found`);
+    return this.categoryRepo.remove(category);
+  }
+}
