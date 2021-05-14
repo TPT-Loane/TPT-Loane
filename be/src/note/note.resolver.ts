add5de17c2d28d469a774929375f24ac88b8c171
tuulingo <oliver.tuuling@tptlive.ee>
2021-05-14 08:42:57 +0300

    Make notes operations functional

--- be/src/note/note.resolver.ts
+++ be/src/note/note.resolver.ts
@@ -2,22 +2,23 @@ import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { Inject } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { Note } from './entities/note.entity';
+import { CreateNoteInput } from './dto/create-note.input';
 
-@Resolver((of) => Note)
+@Resolver(() => Note)
 export class NoteResolver {
   constructor(@Inject(NoteService) private noteService: NoteService) {}
 
-  @Query((returns) => Note)
-  async item(@Args('id') id: number): Promise<Note> {
+  @Query(returns => Note)
+  async note(@Args('id') id: number): Promise<Note> {
     return await this.noteService.findOne(id);
   }
 
-  @Query((returns) => [Note])
-  async items(): Promise<Note[]> {
+  @Query(returns => [Note])
+  async notes() {
     return await this.noteService.findAll();
   }
-  @Mutation((returns) => Note)
-  async createItem(@Args('content') content: string): Promise<Note> {
-    return this.noteService.createNote({ content });
+  @Mutation(returns => Note)
+  async createNote(@Args('createNoteInput') createNoteInput: CreateNoteInput) {
+    return this.noteService.createNote(createNoteInput);
   }
 }
