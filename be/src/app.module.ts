e0f421250357e4f073c87ea55a917a510991765d
tuulingo <oliver.tuuling@tptlive.ee>
2021-05-14 08:42:57 +0300

    Make notes operations functional

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { TestModule } from './service/test.module';
 import { ConfigModule } from '@nestjs/config';
 import { CategoryModule } from './category/category.module';
+import { NoteModule } from './note/note.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { CategoryModule } from './category/category.module';
       synchronize: true,
     }),
     CategoryModule,
+    NoteModule
   ],
   controllers: [AppController],
 })
