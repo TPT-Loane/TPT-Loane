2dfbc08b2e3d67b18d153ca35b25fe438787a892
tuulingo <oliver.tuuling@tptlive.ee>
2021-05-18 15:18:36 +0300

    Get the item of a note

--- be/src/note/note.service.ts
+++ be/src/note/note.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/item/entities/item.entity';
+import { ItemService } from 'src/item/item.service';
 import { Repository } from 'typeorm';
 import { CreateNoteInput } from './dto/create-note.input';
 import { Note } from './entities/note.entity';
 
 @Injectable()
 export class NoteService {
-  constructor(@InjectRepository(Note) private notesRepository: Repository<Note>) { }
+  constructor(@InjectRepository(Note) private notesRepository: Repository<Note>,private itemService: ItemService) { }
 
   createNote(createNoteInput: CreateNoteInput): Promise<Note> {
     const newNote = this.notesRepository.create(createNoteInput);
@@ -24,6 +26,10 @@ export class NoteService {
 
   remove(id: number) {
     return this.notesRepository.delete(id);
+  }
+
+  getItem(itemId: number): Promise<Item> {
+    return this.itemService.findOne(itemId);
   }
   
 }
