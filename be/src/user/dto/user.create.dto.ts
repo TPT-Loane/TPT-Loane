9d9d6c3e7743bdb2ca55002f6115d285d10c328e
Erikyy <erikyl@hotmail.com>
2021-05-18 10:13:01 +0300

    Remove classvalidators and fix some DTO gql types

--- be/src/user/dto/user.create.dto.ts
+++ be/src/user/dto/user.create.dto.ts
@@ -1,21 +1,17 @@
 import { Role } from '../roles/user.roles';
-import { IsNotEmpty, IsEmail } from 'class-validator';
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
-@ObjectType()
 @InputType()
 export class CreateUserDTO {
-  @IsNotEmpty() @Field() username: string;
-  @IsNotEmpty() @Field() password: string;
+  @Field() username: string;
+  @Field() password: string;
 
-  @IsNotEmpty()
-  @IsEmail()
   @Field()
   email: string;
 
-  @IsNotEmpty() @Field() phone: string;
-  @IsNotEmpty() @Field() full_name: string;
-  @IsNotEmpty() @Field() role: Role;
-  @IsNotEmpty() @Field() registration_date: Date;
-  @IsNotEmpty() @Field() personal_code: string;
+  @Field() phone: string;
+  @Field() full_name: string;
+  @Field() role: Role;
+  @Field() registration_date: Date;
+  @Field() personal_code: string;
 }
