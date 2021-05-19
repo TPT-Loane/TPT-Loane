8d7931bd25799cfe5c1f4465f0926ca82227eba8
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-19 18:22:04 +0300

    made a separate component for Content on headers left side

--- fe/src/components/Header/Header.tsx
+++ fe/src/components/Header/Header.tsx
@@ -1,8 +1,5 @@
-import { Box, Flex, Heading, HStack, useColorModeValue } from '@chakra-ui/react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link, Link as RouteLink } from 'react-router-dom';
-import { NAV_ITEMS } from '../../utils';
-import { Drawerbar } from '../Sidebar';
+import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
+import { HeaderLeftSide } from './HeaderLeftSide';
 
 export default function Header(): JSX.Element {
   return (
@@ -14,35 +11,7 @@ export default function Header(): JSX.Element {
       top={0}
     >
       <Flex h={16} alignItems="center" justifyContent="space-between">
-        <HStack spacing={8} alignItems="center">
-          <Drawerbar />
-          <Heading as={Link} size="md" to="/">
-            TPT-Loane
-          </Heading>
-          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
-            {NAV_ITEMS.map(navItem => {
-              const { href, iconName } = navItem;
-              const newIconName = iconName;
-              return (
-                <RouteLink key={href} to={href}>
-                  <Box
-                    px={2}
-                    py={1}
-                    rounded="md"
-                    _hover={{
-                      textDecoration: 'none',
-                      bg: useColorModeValue('gray.200', 'gray.700'),
-                    }}
-                  >
-                    <Box textAlign="center" minW="2em">
-                      <FontAwesomeIcon icon={['fas', newIconName]} size="lg" />
-                    </Box>
-                  </Box>
-                </RouteLink>
-              );
-            })}
-          </HStack>
-        </HStack>
+        <HeaderLeftSide />
       </Flex>
     </Box>
   );
---
+++ fe/src/components/Header/HeaderLeftSide.tsx
@@ -0,0 +1,38 @@
+import { HStack, Heading, Box, useColorModeValue } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { NAV_ITEMS } from '../../utils';
+import { Drawerbar } from '../Sidebar';
+
+export const HeaderLeftSide: React.FC = () => (
+  <HStack spacing={8} alignItems="center">
+    <Drawerbar />
+    <Heading as={Link} size="md" to="/">
+      TPT-Loane
+    </Heading>
+    <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
+      {NAV_ITEMS.map(navItem => {
+        const { href, iconName } = navItem;
+        const newIconName = iconName;
+        return (
+          <Link key={href} to={href}>
+            <Box
+              px={2}
+              py={1}
+              rounded="md"
+              _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('gray.200', 'gray.700'),
+              }}
+            >
+              <Box textAlign="center" minW="2em">
+                <FontAwesomeIcon icon={['fas', newIconName]} size="lg" />
+              </Box>
+            </Box>
+          </Link>
+        );
+      })}
+    </HStack>
+  </HStack>
+);
