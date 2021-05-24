2c3d107148c95c304e9af95cec6a642839b15f52
Kahuka <kaurkristjan.rajasaar@tptlive.ee>
2021-05-24 10:42:40 +0300

    FE Add possibility to switch between "dark" and "light" theme fix

--- fe/src/components/Header/Header.tsx
+++ fe/src/components/Header/Header.tsx
@@ -1,20 +1,13 @@
 import {
   Box,
   Flex,
-  Heading,
   HStack,
   useColorModeValue,
-  Text,
-  useColorMode,
 } from '@chakra-ui/react';
-import { Link as RouteLink } from 'react-router-dom';
-import { NAV_ITEMS } from '../../utils';
-import { Drawerbar } from '../Sidebar';
 import ColorMode from '../ColorMode';
 import { HeaderLeftSide } from './HeaderLeftSide';
 
 export default function Header(): JSX.Element {
-  const { colorMode } = useColorMode();
   return (
     <Box
       bg={useColorModeValue('gray.100', 'gray.900')}
@@ -24,34 +17,10 @@ export default function Header(): JSX.Element {
       top={0}
     >
       <Flex h={16} alignItems="center" justifyContent="space-between">
-        <HStack spacing={8} alignItems="center">
-          <Drawerbar />
-          <Heading as="h4" size="md">
-            TPT-Loane
-          </Heading>
-          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
-            {NAV_ITEMS.map(navItem => (
-              <RouteLink key={navItem.href} to={navItem.href}>
-                <Box
-                  px={2}
-                  py={1}
-                  rounded="md"
-                  _hover={{
-                    textDecoration: 'none',
-                    bg: useColorModeValue('gray.200', 'gray.700'),
-                  }}
-                >
-                  <Text fontSize="xl">{navItem.label}</Text>
-                </Box>
-              </RouteLink>
-            ))}
-          </HStack>
-        </HStack>
+        <HeaderLeftSide />
         <HStack spacing={12} ml="auto">
           <ColorMode />
         </HStack>
-        <HStack />
-        <HeaderLeftSide />
       </Flex>
     </Box>
   );
