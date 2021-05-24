30581f5d3972730b35e085ef4c4751cb631ae20b
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-24 12:42:44 +0300

    fix merge changes

--- fe/src/components/Header/Header.tsx
+++ fe/src/components/Header/Header.tsx
@@ -11,41 +11,7 @@ export default function Header(): JSX.Element {
       top={0}
     >
       <Flex h={16} alignItems="center" justifyContent="space-between">
-<<<<<<< HEAD
-        <HStack spacing={8} alignItems="center">
-          <Drawerbar />
-          <Heading as="h4" size="md">
-            TPT-Loane
-          </Heading>
-          <HStack
-            as="nav"
-            spacing={4}
-            display={{ base: 'none', md: 'flex' }}
-          >
-            {NAV_ITEMS.map(navItem => (
-              <RouteLink
-                key={navItem.href}
-                to={navItem.href}
-                aria-label="Navbar route"
-              >
-                <Box
-                  px={2}
-                  py={1}
-                  rounded="md"
-                  _hover={{
-                    textDecoration: 'none',
-                    bg: useColorModeValue('gray.200', 'gray.700'),
-                  }}
-                >
-                  <Text fontSize="xl">{navItem.label}</Text>
-                </Box>
-              </RouteLink>
-            ))}
-          </HStack>
-        </HStack>
-=======
         <HeaderLeftSide />
->>>>>>> main
       </Flex>
     </Box>
   );
