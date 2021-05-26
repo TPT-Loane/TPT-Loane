254b27a2f761ba46e3f2a96a788f87f53584963b
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-26 15:29:39 +0300

    add login and register links to navbar

--- fe/src/components/Header/Header.tsx
+++ fe/src/components/Header/Header.tsx
@@ -2,8 +2,10 @@ import {
   Box,
   Flex,
   HStack,
+  Text,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import ColorMode from '../ColorMode';
 import { HeaderLeftSide } from './HeaderLeftSide';
 
@@ -16,10 +18,38 @@ export default function Header(): JSX.Element {
       zIndex={3}
       top={0}
     >
+      {/* @todo navbar layout should depend on whether the user is logged in or not */}
       <Flex h={16} alignItems="center" justifyContent="space-between">
         <HeaderLeftSide />
+        {/* @todo move this to HeaderRightSide component */}
         <HStack spacing={12} ml="auto">
           <ColorMode />
+          <Link to="login">
+            <Box
+              px={2}
+              py={1}
+              rounded="md"
+              _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('gray.200', 'gray.700'),
+              }}
+            >
+              <Text fontSize="xl">Login</Text>
+            </Box>
+          </Link>
+          <Link to="register">
+            <Box
+              px={2}
+              py={1}
+              rounded="md"
+              _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('gray.200', 'gray.700'),
+              }}
+            >
+              <Text fontSize="xl">Register</Text>
+            </Box>
+          </Link>
         </HStack>
       </Flex>
     </Box>
