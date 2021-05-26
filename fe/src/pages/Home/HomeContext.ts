2dcbe3cf52709eee984c0bb534aaee0fa1cb1e6d
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2021-05-26 13:05:39 +0300

    Add search string and filters values to HomeContext

--- fe/src/pages/Home/HomeContext.ts
+++ fe/src/pages/Home/HomeContext.ts
@@ -6,6 +6,10 @@ interface HomeContextType {
   setView: React.Dispatch<React.SetStateAction<ViewType>>;
   page: number;
   setPage: React.Dispatch<React.SetStateAction<number>>;
+  searchTerm: string;
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+  filters: Array<number>;
+  setFilters: React.Dispatch<React.SetStateAction<Array<number>>>;
 }
 
 export const initHomeContext: HomeContextType = {
@@ -13,6 +17,10 @@ export const initHomeContext: HomeContextType = {
   setView: () => undefined,
   page: 1,
   setPage: () => undefined,
+  searchTerm: '',
+  setSearchTerm: () => undefined,
+  filters: [],
+  setFilters: () => undefined,
 };
 
 export const HomeContext = createContext<HomeContextType>(initHomeContext);
