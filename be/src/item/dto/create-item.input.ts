6c9598c4e5cc65c9fdacacebd99d435d513af6ca
SimkaBoi <simkaviiner@gmail.com>
2021-05-27 17:18:54 +0300

    Add productId field to create-item input

--- be/src/item/dto/create-item.input.ts
+++ be/src/item/dto/create-item.input.ts
@@ -1,7 +1,10 @@
-import { InputType, Field } from '@nestjs/graphql';
+import { InputType, Field, Int } from '@nestjs/graphql';
 
 @InputType()
 export class CreateItemInput {
   @Field()
   regCode: string;
+
+  @Field(() => Int)
+  productId: number;
 }
