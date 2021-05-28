8e4b15f1ef122856426363c5f45a43dede6a5110
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-28 11:08:09 +0300

    restyle header for grid view, make it responsive

--- fe/src/components/Header/Header.tsx
+++ fe/src/components/Header/Header.tsx
@@ -3,9 +3,16 @@ import { HeaderLeftSide } from './HeaderLeftSide';
 import { HeaderRightSide } from './HeaderRightSide';
 
 export default function Header(): JSX.Element {
-  const me = 'Peeter';
+  const me = undefined;
   return (
-    <Flex height="100%" align="center" maxW={1200} m="auto">
+    <Flex
+      height="100%"
+      align="center"
+      maxW={{
+        base: 400, sm: 500, md: 700, lg: 900, xl: 1200,
+      }}
+      m="auto"
+    >
       <HeaderLeftSide />
       <HeaderRightSide me={me} />
     </Flex>
