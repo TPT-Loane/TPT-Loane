ddd676d76d72fbf78fb2ac5a9fbfff4c64611398
SimkaBoi <simkaviiner@gmail.com>
2021-05-28 11:23:02 +0300

    Import modules using forwardRef

--- be/src/product/product.module.ts
+++ be/src/product/product.module.ts
@@ -4,9 +4,10 @@ import { ProductResolver } from './product.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { ItemModule } from 'src/item/item.module';
+import { CategoryModule } from 'src/category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product]), forwardRef(() => ItemModule)],
+  imports: [TypeOrmModule.forFeature([Product]), forwardRef(() => ItemModule), forwardRef(() => CategoryModule)],
   providers: [ProductResolver, ProductService],
   exports: [ProductService],
 })
