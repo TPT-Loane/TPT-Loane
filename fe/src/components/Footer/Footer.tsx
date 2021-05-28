854ca08b9fe21fb6ef86b17ac6bf4f5cd26f37be
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-28 11:07:51 +0300

    restyle footer for grid view

--- fe/src/components/Footer/Footer.tsx
+++ fe/src/components/Footer/Footer.tsx
@@ -1,36 +1,32 @@
 import {
-  Box,
-  Container,
-  Link,
-  Stack,
-  TextProps,
-  useColorModeValue,
+  Box, Flex, Link, Stack, TextProps,
 } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { Copyright } from './CopyRight';
 
 export default function Footer(props: TextProps): JSX.Element {
   return (
-    <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}
-      {...props}
-    >
-      <Container
-        as={Stack}
-        direction={{ base: 'column', md: 'row' }}
-        spacing={4}
-        justify={{ base: 'center', md: 'space-between' }}
-        align={{ base: 'center', md: 'center' }}
+    <Box {...props} padding={5}>
+      <Flex
+        flexDirection={{ base: 'column', lg: 'row' }}
+        justify={{ base: 'center', lg: 'space-between' }}
+        align={{ base: 'center', lg: 'center' }}
+        textAlign="center"
+        maxW={1100}
+        m="auto"
       >
-        <Copyright />
-        <Stack direction="row" spacing={6}>
-          <Link href="https://www.tptlive.ee/" isExternal>
-            Tallinn Polytechnic School
-            <ExternalLinkIcon mx="2px" />
-          </Link>
-        </Stack>
-      </Container>
+        <Box>
+          <Copyright />
+        </Box>
+        <Box>
+          <Stack direction="row" spacing={6}>
+            <Link href="https://www.tptlive.ee/" isExternal>
+              Tallinn Polytechnic School
+              <ExternalLinkIcon mx="2px" />
+            </Link>
+          </Stack>
+        </Box>
+      </Flex>
     </Box>
   );
 }
