dc796abd1b455e70deeb91ff62d3adde084a5d9c
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-28 11:07:32 +0300

    fix sidebar for grid view

--- fe/src/components/Sidebar/Sidebar.tsx
+++ fe/src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,9 @@
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 import { SidebarContent } from './SidebarContent';
 
 const Sidebar: React.FC = () => (
-  <Box
-    display={{ base: 'none', md: 'block' }}
-    zIndex={2}
-    position="sticky"
-    overflowY="auto"
-    bg={useColorModeValue('gray.100', 'gray.900')}
-  >
+  <Box>
     <SidebarContent />
   </Box>
 );
