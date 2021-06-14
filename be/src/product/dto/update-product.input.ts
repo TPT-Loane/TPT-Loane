4dce0c27a3e3b71a40b5e7778a9dc3ce8cb8bad3
SimkaBoi <simkaviiner@gmail.com>
2021-06-14 15:05:29 +0300

    Edit update inputs for category and product

--- be/src/product/dto/update-product.input.ts
+++ be/src/product/dto/update-product.input.ts
@@ -1,8 +1,12 @@
 import { CreateProductInput } from './create-product.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { GraphQLInt } from 'graphql';
 
 @InputType()
 export class UpdateProductInput extends PartialType(CreateProductInput) {
   @Field(() => Int)
   id: number;
+
+  @Field(type => [GraphQLInt], { nullable: true })
+  categories: number[] | null;
 }
