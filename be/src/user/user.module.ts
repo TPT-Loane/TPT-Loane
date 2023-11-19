fc12a5e8f9e10a99723a27138cb4690f71f98fc1
Virgo Frank <virgofrank2@gmail.com>
2023-11-19 21:58:56 +0200

    Add user controller

--- be/src/user/user.module.ts
+++ be/src/user/user.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserResolver } from './resolvers/user.resolver';
 import { UserService } from './service/user.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService, UserResolver],
   exports: [UserService],
+  controllers: [UserController]
 })
 export class UserModule {}
